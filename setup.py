73ae76c3a42737115e8820405e674eddfcd15f42
Jake Nesbitt <jjnesbitt2@gmail.com>
2019-07-18 15:53:04 -0400

    Increment version for bug fix

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ with open('README.md') as readme:
 
 setup(
     name='girder-geobrowser',
-    version='0.2.2',
+    version='0.2.3',
     description='Map-oriented custom Girder app'
                 'for geospatial metadata browsing',
     long_description=long_description,
