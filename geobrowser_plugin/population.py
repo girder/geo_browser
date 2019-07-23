a9058c5f6d2b14961597124bc681b87bdebe4c60
Jake Nesbitt <jjnesbitt2@gmail.com>
2019-07-23 12:56:46 -0400

    Add path option to population script

--- geobrowser_plugin/population.py
+++ geobrowser_plugin/population.py
@@ -3,21 +3,30 @@ import json
 from bson.objectid import ObjectId
 
 from girder.models.collection import Collection
+from girder.utility.path import lookUpPath
 
 
-@click.command(name="populate-collection-meta",
+@click.command(name='populate-collection-meta',
                short_help='Populates a collection with the provided metadata',
                help='Populates a collection\'s meta field with '
                     'the provided JSON data')
-@click.option('-i', '--id', multiple=True, required=True,
+@click.option('-i', '--id', 'ids', multiple=True,
               help="Used to specify a collection ID to extract metadata to.")
+@click.option('-p', '--path', 'paths', multiple=True,
+              help="Used to specify a girder path to extract metadata to. "
+              "Default behavior is to create this path if it doesn't exist")
 @click.option('-d', '--data', required=True,
               help="The metadata to populate the desired collection(s) with.")
-def populate(id, data):
+def populate(ids, paths, data):
+    if not (ids or paths):
+        click.echo('Error: No destination specified')
+        return
+
     data = json.load(open(data, 'r'))
+    totalTargets = len(ids) + len(paths)
     success = 0
 
-    for collectionId in id:
+    for collectionId in ids:
         collection = Collection().findOne({
             '_id': ObjectId(collectionId)
         })
@@ -28,5 +37,15 @@ def populate(id, data):
         else:
             click.echo('Warning: No collection found with ID: ' + collectionId)
 
+    for path in paths:
+        doc = lookUpPath(path, force=True)
+        if doc['model'] != 'collection':
+            click.echo('Warning: Ignoring non-collection path: ' + path)
+            continue
+
+        doc = doc['document']
+        Collection().setMetadata(collection=doc, metadata=data)
+        success += 1
+
     click.echo('Successfully set metadata on '
-               + str(success) + '/' + str(len(id)) + ' collections')
+               + str(success) + '/' + str(totalTargets) + ' targets')
