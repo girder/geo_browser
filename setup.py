117c30f8a69f8ca7d947f587b2a06b209b200cac
Jake Nesbitt <jjnesbitt2@gmail.com>
2019-08-23 16:05:08 -0400

    Increment version for pypi release

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ with open('README.md') as readme:
 
 setup(
     name='girder-geobrowser',
-    version='0.4.5',
+    version='0.4.6',
     description='Map-oriented custom Girder app'
                 'for geospatial metadata browsing',
     long_description=long_description,
